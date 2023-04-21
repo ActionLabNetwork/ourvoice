1d5f7a26c0c9860e279081b1ca50a5612be4e74e
Christopher <christopherowenprawira@gmail.com>
2023-04-21 19:02:44 +1000

    refactor(create-post-ui): rename variables

--- apps/ourvoice-app/src/components/post/CreatePost.vue
+++ apps/ourvoice-app/src/components/post/CreatePost.vue
@@ -3,12 +3,11 @@
     <div class="container mx-auto p-4">
       <div
         class="bg-white rounded-lg shadow-md p-8 max-w-lg mx-auto"
-        @submit.prevent="submitForm"
       >
-        <form>
+        <form @submit.prevent="submitForm" class="space-y-6">
           <h2 class="text-2xl font-semibold mb-6 text-gray-800">Create Post</h2>
-          <div class="mb-6">
-            <label for="title" class="block text-gray-700 mb-2 text-md">Title</label>
+          <div class="flex flex-col space-y-2">
+            <label for="title" class="block text-gray-700 mb-1 font-semibold">Title</label>
             <div class="flex">
               <span class="bg-gray-200 p-2 rounded-l-md">
                 <font-awesome-icon :icon="['fas', 'heading']" class="icon-color" />
@@ -18,12 +17,12 @@
                 type="text"
                 id="title"
                 placeholder="Share your thoughts anonymously"
-                class="w-full border border-solid border-gray-300 rounded-md rounded-l-none px-4 py-2 focus:border-blue-500 focus:ring-blue-500 outline-none transition duration-200 font-semibold"
+                class="w-full border border-solid border-gray-300 rounded-md rounded-l-none px-4 py-2 focus:border-blue-500 focus:ring-blue-500 outline-none transition duration-200 font-semibold text-gray-800"
               />
             </div>
           </div>
-          <div class="mb-6">
-            <label for="content" class="block text-gray-700 text-md mb-2">Content</label>
+          <div class="flex flex-col space-y-2">
+            <label for="content" class="block text-gray-700 font-semibold mb-1">Content</label>
             <textarea
               id="content"
               v-model="content"
@@ -32,17 +31,17 @@
               placeholder="I have an idea for improving..."
               @input="updateCharacterCount"
             ></textarea>
-            <div class="flex flex-col text-right">
-              <span class="text-gray-500">{{ characterCount }} / 50</span>
+            <div class="flex flex-row-reverse justify-between text-sm">
+              <span :class="contentError ? 'text-red-500' : 'text-green-500'">{{ characterCount }} / 50</span>
               <span class="text-red-500">{{ contentError }}</span>
             </div>
           </div>
-          <div class="mb-6">
-            <label for="categories" class="block text-gray-700 text-md mb-2">Categories</label>
-            <Multiselect id="categories" v-model="selectedCategories" :options="categories.data.map(({ name }) => name)" mode="tags" :searchable="true" required :caret="true" placeholder="Select categories" class="px-4 multiselect-blue" />
+          <div class="flex flex-col space-y-2">
+            <label for="categories" class="block text-gray-700 font-semibold mb-1">Categories</label>
+            <Multiselect id="categories" v-model="selectedCategories" :options="categoriesData.data.map(({ name }) => name)" mode="tags" :searchable="true" required :caret="true" placeholder="Select categories" class="px-4 multiselect-blue" />
           </div>
-          <div class="mb-6">
-            <label for="attachments" class="block text-gray-700 text-md mb-2">Attachments</label>
+          <div class="flex flex-col space-y-2">
+            <label for="attachments" class="block text-gray-700 font-semibold mb-1">Attachments</label>
             <div class="flex">
               <span class="bg-gray-200 px-2 py-3 rounded-l-md">
                 <font-awesome-icon :icon="['fas', 'paperclip']" class="icon-color" />
@@ -55,11 +54,18 @@
                 multiple
                 />
             </div>
+            <div class="mt-2">
+              <p class="font-semibold text-sm mb-1">Uploaded Attachments:</p>
+              <ul class="list-inside list-disc">
+                <li v-for="(attachment, index) in attachments" :key="index" class="text-sm">{{ attachment.name }}</li>
+              </ul>
+            </div>
           </div>
           <div class="flex justify-end">
             <button
               type="submit"
-              class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-lg shadow-md flex items-center transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              :disabled="!isValidForm"
+              class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-lg shadow-md flex items-center transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:bg-indigo-300 disabled:cursor-not-allowed"
             >
               Create Post
             </button>
@@ -71,7 +77,7 @@
 </template>
 
 <script lang="ts">
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import Multiselect from '@vueform/multiselect';
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
@@ -98,8 +104,7 @@ export default {
 
     const title = ref('');
     const content = ref('');
-    const category = ref('');
-    const categories = reactive<CategoriesObjType>({
+    const categoriesData = reactive<CategoriesObjType>({
       data: [],
       loading: false,
       error: undefined
@@ -108,6 +113,15 @@ export default {
     const attachments = ref<FileList | null>(null);
     const characterCount = ref(0)
     const contentError = ref('')
+
+    const isValidForm = computed(() => {
+      const hasTitle = !!title.value
+      const hasContent = !!content.value
+      const hasContentError = !!contentError.value
+      const hasCategories = !!selectedCategories.value.length
+
+      return hasTitle && hasContent && !hasContentError && hasCategories
+    })
 
     const updateCharacterCount = () => {
       characterCount.value = content.value.length
@@ -123,32 +137,32 @@ export default {
       // Perform form validation, data processing, and API calls here.
       console.log("Title:", title.value);
       console.log("Content:", content.value);
-      console.log("Category:", category.value);
+      console.log("Categories:", selectedCategories.value);
       console.log("Attachments:", attachments.value);
 
       // After successfully submitting the form, reset the form fields
       title.value = '';
       content.value = '';
-      category.value = '';
+      selectedCategories.value = [];
       attachments.value = null;
     };
 
     onResult(({ data, loading, error }) => {
-      categories.data = data.categories.map(({ id, name }) => ({ id, name }))
-      categories.loading = loading
-      categories.error = error
+      categoriesData.data = data.categories.map(({ id, name }) => ({ id, name }))
+      categoriesData.loading = loading
+      categoriesData.error = error
     });
 
     return {
       title,
       content,
-      category,
-      categories,
+      categoriesData,
       selectedCategories,
       attachments,
       updateCharacterCount,
       characterCount,
       contentError,
+      isValidForm,
       updateAttachments,
       submitForm,
     };
