7d9a43f71efad9cca359c7fc06767de82c0fbcc5
Christopher <christopherowenprawira@gmail.com>
2023-04-24 10:43:33 +1000

    refactor(form-input): add forminput component

---
+++ apps/ourvoice-app/src/components/inputs/FormInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col space-y-2">
+    <label :for="id" class="block text-gray-700 mb-1 font-semibold">{{ labelText }}</label>
+    <div class="flex">
+      <slot name="icon"></slot>
+      <slot></slot>
+    </div>
+
+    <div v-if="error" class="text-red-500 text-sm">{{ error }}</div>
+    <slot name="info"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    id: String,
+    labelText: String,
+    error: String,
+  },
+});
+</script>
