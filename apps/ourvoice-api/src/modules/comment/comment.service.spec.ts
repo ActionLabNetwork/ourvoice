16d5e7da7b9cc56d8ca35b783e5ab3c463cc77c7
Mingye1Li <87420384+Mingye1Li@users.noreply.github.com>
2023-05-02 11:38:15 +1000

    test(comment-service): add unit tests for comment.service

---
+++ apps/ourvoice-api/src/modules/comment/comment.service.spec.ts
@@ -0,0 +1,139 @@
+import { PrismaService } from '../../database/prisma.service';
+import { CommentService } from './comment.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CommentRepository } from './comment.repository';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Comment } from '@prisma/client';
+
+describe('CommentService', () => {
+  let commentService: CommentService;
+  let commentRepositoryMock: DeepMocked<CommentRepository>;
+
+  const dummyComment: Comment = {
+    id: 1,
+    content: 'Test Content',
+    authorId: 1,
+    createdAt: new Date(),
+    disabledAt: null,
+    moderatedAt: null,
+    publishedAt: null,
+    moderated: false,
+    published: false,
+    postId: 0,
+    parentId: 0,
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CommentRepository,
+          useValue: createMock<CommentRepository>(),
+        },
+        CommentService,
+        PrismaService,
+      ],
+    }).compile();
+
+    commentService = module.get<CommentService>(CommentService);
+    commentRepositoryMock = module.get(CommentRepository);
+  });
+
+  it('should create a comment', async () => {
+    // Arrange
+    const commentCreateInput = {
+      content: 'Test content',
+      authorId: 1,
+      postId: 1,
+    };
+
+    commentRepositoryMock.createComment.mockResolvedValue(dummyComment);
+
+    // Act
+    const result = await commentService.createComment(commentCreateInput);
+
+    // Assert
+    expect(result).toEqual(dummyComment);
+
+    const { authorId, postId, ...restData } = commentCreateInput;
+    expect(commentRepositoryMock.createComment).toHaveBeenCalledWith({
+      ...restData,
+      author: { connect: { id: authorId } },
+      post: postId ? { connect: { id: postId } } : undefined,
+    });
+  });
+
+  it('should get comments by ID', async () => {
+    // Arrange
+    const commentId = 1;
+
+    commentRepositoryMock.getCommentById.mockResolvedValue(dummyComment);
+
+    // Act
+    const result = await commentService.getCommentById(commentId);
+
+    // Assert
+    expect(result).toEqual(dummyComment);
+    expect(commentRepositoryMock.getCommentById).toHaveBeenCalledWith(
+      commentId,
+    );
+  });
+
+  it('should update a comment', async () => {
+    // Arrange
+    const commentId = 1;
+    const commentUpdateInput = {
+      content: 'Updated Content',
+    };
+
+    const updatedComment = {
+      ...dummyComment,
+      content: commentUpdateInput.content,
+    };
+
+    commentRepositoryMock.updateComment.mockResolvedValue(updatedComment);
+
+    // Act
+    const result = await commentService.updateComment(
+      commentId,
+      commentUpdateInput,
+    );
+
+    // Assert
+    expect(result).toEqual(updatedComment);
+    expect(commentRepositoryMock.updateComment).toHaveBeenCalledWith(
+      commentId,
+      commentUpdateInput,
+    );
+  });
+
+  it('should delete a comment', async () => {
+    // Arrange
+    const commentId = 1;
+
+    commentRepositoryMock.deleteComment.mockResolvedValue(dummyComment);
+
+    // Act
+    const result = await commentService.deleteComment(commentId);
+
+    // Assert
+    expect(result).toEqual(dummyComment);
+    expect(commentRepositoryMock.deleteComment).toHaveBeenCalledWith(commentId);
+  });
+
+  it('should return null for non-existent comment ID', async () => {
+    // Arrange
+    const commentId = 999;
+
+    commentRepositoryMock.getCommentById.mockResolvedValue(null);
+
+    // Act
+    const result = await commentService.getCommentById(commentId);
+
+    // Assert
+    expect(result).toBeNull();
+    expect(commentRepositoryMock.getCommentById).toHaveBeenCalledWith(
+      commentId,
+    );
+  });
+});
