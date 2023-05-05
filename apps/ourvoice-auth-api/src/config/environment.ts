cf6918e2132541d49fcb966e5c4597014e21e0c3
Aare Puussaar <aare.puussaar@gmail.com>
2023-05-05 08:58:25 +0100

    fix: cleanup unused code

--- apps/ourvoice-auth-api/src/config/environment.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-import dotenv from 'dotenv';
-
-export default () => {
-  console.log('ENV');
-  return dotenv.parse(readFileSync('../../.env')) as Record<string, any>;
-};
