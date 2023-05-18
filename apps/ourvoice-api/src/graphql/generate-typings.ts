832529d9ad4dcc00513556987227bec09a9d13fa
knthmn <knthmn0@gmail.com>
2023-05-18 13:23:48 +1000

    chore(api): improve generate-typings.ts
    - make it possible to run from any working directory
    - prevent accidental inclusion of .graphql files in node_modules etc

--- apps/ourvoice-api/src/graphql/generate-typings.ts
+++ apps/ourvoice-api/src/graphql/generate-typings.ts
@@ -2,8 +2,9 @@ import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
+process.chdir(__dirname);
 definitionsFactory.generate({
-  typePaths: ['../**/*.graphql'],
+  typePaths: ['./schemas/*.graphql', '../**/modules/**/*.graphql'],
   path: join(process.cwd(), '../graphql.ts'),
   outputAs: 'class',
   watch: true,
