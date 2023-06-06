d1f504d815b73fc6ef7d4b07bc23b848c9f83280
knthmn <knthmn0@gmail.com>
2023-06-06 18:53:04 +1000

    fix(portal): fix card section scroll behavior

--- apps/ourvoice-portal/components/CardsSection.vue
+++ apps/ourvoice-portal/components/CardsSection.vue
@@ -1,45 +1,32 @@
 <template>
-  <div ref="containerRef" class="relative top-0">
-    <!-- Moving container-->
-    <div
-      ref="sideescrollRef"
-      class="h-screen w-screen flex flex-col justify-center bg-ourvoice-portal-yellow"
-      :class="sidescrollStyle"
-    >
-      <!-- Heading -->
-      <div class="container flex flex-col px-2 items-center">
-        <h1 class="max-w-[708px] font-Inter">
-          {{ cardTitle }}
-        </h1>
-        <p
-          class="text-center font-Inter font-normal text-xl mt-4 max-w-[600px]"
-        >
-          {{ cardSubTitle }}
-        </p>
-      </div>
-      <!-- Cards -->
-      <div :style="cardsStyle">
-        <!--- Horizontal Scroller --->
-        <div
-          class="flex flex-row flex-nowrap w-fit overflow-x-visible gap-4 lg:gap-8 mt-16 px-10 lg:px-36"
-          ref="sidescrollRef"
-        >
-          <ClientOnly>
-            <card
-              v-for="(feature, idx) in features"
-              :id="idx"
-              :key="idx"
-              :feature="feature"
-              :class="feature.url ? 'cursor-pointer' : 'cursor-auto'"
-              @click="onClick(feature.url)"
-            />
-          </ClientOnly>
-        </div>
-      </div>
+  <!-- Yellow backbround-->
+  <div
+    class="w-screen py-[142px] flex flex-col justify-center bg-ourvoice-portal-yellow"
+  >
+    <!-- Heading -->
+    <div class="container flex flex-col px-2 items-center">
+      <h1 class="max-w-[708px] font-Inter">
+        {{ cardTitle }}
+      </h1>
+      <p class="text-center font-Inter font-normal text-xl mt-4 max-w-[600px]">
+        {{ cardSubTitle }}
+      </p>
     </div>
-    <!-- Spacing divs -->
-    <div class="h-screen" />
-    <div :style="bottomPadderStyle" />
+    <div
+      class="flex flex-row px-10 lg:px-36 gap-4 lg:gap-8 w-full overflow-x-scroll mt-8 lg:mt-16"
+      ref="sidescrollRef"
+    >
+      <ClientOnly>
+        <card
+          v-for="(feature, idx) in features"
+          :id="idx"
+          :key="idx"
+          :feature="feature"
+          :class="feature.url ? 'cursor-pointer' : 'cursor-auto'"
+          @click="onClick(feature.url)"
+        />
+      </ClientOnly>
+    </div>
   </div>
 </template>
 
@@ -48,7 +35,6 @@ import amplifyImgUrl from '@/assets/img/card-amplify.png'
 import deliverImgUrl from '@/assets/img/card-deliver.png'
 import engageImgUrl from '@/assets/img/card-engage.png'
 import supportImgUrl from '@/assets/img/card-support.png'
-import { useElementBounding, useMounted, useWindowSize } from '@vueuse/core'
 import { ref } from 'vue'
 
 import Card from './Card.vue'
@@ -108,54 +94,30 @@ export default defineComponent({
         image: supportImgUrl,
       },
     ])
-
-    const containerRef = ref(null)
-    const containerBBox = useElementBounding(containerRef)
     const sidescrollRef = ref(null)
-    const sidescrollBBox = useElementBounding(sidescrollRef)
-    const viewportSize = useWindowSize()
 
-    const bottomPadderStyle = computed(() => ({
-      height: sidescrollBBox.width.value - viewportSize.width.value + 'px',
-    }))
-    const containerPosition = computed(() =>
-      containerBBox.top.value > 0
-        ? 'before'
-        : containerBBox.bottom.value >= viewportSize.height.value
-        ? 'inside'
-        : 'after'
-    )
-    const isMounted = useMounted()
-    const sidescrollStyle = computed(() =>
-      !isMounted.value || containerPosition.value == 'before'
-        ? 'absolute top-0'
-        : containerPosition.value == 'inside'
-        ? 'fixed top-0'
-        : 'absolute bottom-0'
-    )
-    const coercePush = (value: number) => {
-      let coercedValue = Math.max(0, -containerBBox.top.value)
-      coercedValue = Math.min(
-        coercedValue,
-        sidescrollBBox.width.value - viewportSize.width.value
-      )
-      return coercedValue
-    }
-    const cardsStyle = computed(() => ({
-      right: coercePush(-containerBBox.top.value) + 'px',
-      position: 'relative',
-    }))
+    onMounted(() => {
+      sidescrollRef.value.addEventListener('wheel', (event) => {
+        const contentWidth = sidescrollRef.value.scrollWidth
+        const containerWidth = sidescrollRef.value.offsetWidth
+        const scrollPosition = sidescrollRef.value.scrollLeft
+        if (
+          (event.deltaY < 0 && scrollPosition <= 0) ||
+          (event.deltaY > 0 && scrollPosition + containerWidth >= contentWidth)
+        ) {
+          return
+        }
+        event.preventDefault()
+        sidescrollRef.value.scrollLeft += event.deltaY + event.deltaX
+      })
+    })
 
     return {
       features,
       cardTitle,
       cardSubTitle,
       onClick,
-      containerRef,
       sidescrollRef,
-      sidescrollStyle,
-      bottomPadderStyle,
-      cardsStyle,
     }
   },
 })
