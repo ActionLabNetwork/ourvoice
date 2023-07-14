de9740f89021e198658278c7754181ace03f981e
Mingye1Li <87420384+Mingye1Li@users.noreply.github.com>
2023-07-14 13:31:18 +1000

    style(app): add reusable back button

---
+++ apps/ourvoice-app/src/components/common/BackButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    @click="handleBackBtnClicked"
+    class="inline-flex font-semibold items-center gap-2 transform duration-200 hover:gap-[13px]"
+  >
+    <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M15 7H2M7 1L1.70711 6.29289C1.31658 6.68342 1.31658 7.31658 1.70711 7.70711L7 13"
+        stroke="#111111"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+    Go Back
+  </button>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const handleBackBtnClicked = () => {
+  router.back()
+}
+</script>
--- apps/ourvoice-app/src/views/CreatePostView.vue
+++ apps/ourvoice-app/src/views/CreatePostView.vue
@@ -1,39 +1,13 @@
 <template>
   <div class="mx-auto p-5 w-full min-h-screen bg-gray-200 overflow-y-scroll">
-    <button @click="handleBackBtnClicked" class="inline-flex items-center gap-2">
-      <svg
-        width="16"
-        height="14"
-        viewBox="0 0 16 14"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M15 7H2M7 1L1.70711 6.29289C1.31658 6.68342 1.31658 7.31658 1.70711 7.70711L7 13"
-          stroke="#111111"
-          stroke-width="2"
-          stroke-linecap="round"
-        />
-      </svg>
-      Back
-    </button>
+    <BackButton />
     <Suspense>
-      <!-- <TabsWrapper>
-        <TabContent title="Create Post"> -->
       <CreatePost />
-      <!-- </TabContent>
-      </TabsWrapper> -->
     </Suspense>
   </div>
 </template>
 
 <script setup lang="ts">
 import CreatePost from '../components/post/CreatePost.vue'
-import { useRouter } from 'vue-router'
-// import TabsWrapper from '../components/common/TabsWrapper.vue'
-// import TabContent from '../components/common/TabContent.vue'
-const router = useRouter()
-const handleBackBtnClicked = () => {
-  router.back()
-}
+import BackButton from '@/components/common/BackButton.vue'
 </script>
