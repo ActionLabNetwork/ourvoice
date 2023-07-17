3792a9c7d389654e80fc62e88b1d5e18676d5bcc
Mingye1Li <87420384+Mingye1Li@users.noreply.github.com>
2023-07-17 15:26:06 +1000

    refactor(app): add comment for post on the active thread page

--- apps/ourvoice-app/src/components/comment/CreateComment.vue
+++ apps/ourvoice-app/src/components/comment/CreateComment.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex mt-2 md:mt-4 text-sm md:text-md gap-3" ref="commentWrapper">
-    <div class="flex-none">
+    <div class="flex-none" v-if="props.showAvatar">
       <img
         class="rounded-full w-6 h-6 sm:w-8 sm:h-8"
         :src="`https://ui-avatars.com/api/?size=48?&name=${userStore.nickname}`"
@@ -9,7 +9,7 @@
     <div class="flex-1">
       <textarea
         v-model="input"
-        class="rounded-xl p-5 bg-gray-100 resize-none w-full focus:outline-none max-h-40"
+        class="rounded-3xl p-3 bg-gray-100 resize-none w-full focus:outline-none max-h-40 hover:cursor-pointer focus:cursor-auto"
         :class="[{ 'placeholder:opacity-50': focused }]"
         @focus="handleFocus"
         maxlength="255"
@@ -25,33 +25,57 @@
       </div>
     </div>
 
-    <div v-if="focused || input" class="flex-none mt-2">
+    <div v-if="focused || input" class="flex-none">
       <button
         class="bg-yellow-400 hover:bg-yellow-500 py-3 px-4 rounded-full text-sm"
-        @click="submit"
+        @click="handleSubmit"
       >
         Send
       </button>
     </div>
+    <Toast
+      :type="toastType"
+      :message="toastMessage"
+      class="fixed bottom-0 right-0"
+      v-if="showToast"
+      @click="showToast = false"
+    />
   </div>
-  <!-- {{ typeof input }} -->
-  <!-- {{ focused }} -->
-  <!-- {{ contentValid }} -->
 </template>
-
 <script setup lang="ts">
+import Toast from '@/components/common/Toast.vue'
 import { computed, ref, watch } from 'vue'
 import { useUserStore } from '@/stores/user'
+import { useCommentsStore } from '@/stores/comments'
 import { useTextareaAutosize } from '@vueuse/core'
-import { useFocusWithin } from '@vueuse/core'
-const userStore = useUserStore()
-const emits = defineEmits(['submit'])
-const commentWrapper = ref<HTMLElement | null>(null)
-const { textarea, input } = useTextareaAutosize()
-const { focused } = useFocusWithin(commentWrapper)
 
+const { textarea, input } = useTextareaAutosize()
+import { useFocusWithin } from '@vueuse/core'
+
+const commentsStore = useCommentsStore()
+const userStore = useUserStore()
+
+const props = defineProps({
+  postId: {
+    type: Number || undefined
+  },
+  commentId: {
+    type: Number || undefined
+  },
+  showAvatar: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+const commentWrapper = ref<HTMLElement | null>(null)
+const { focused } = useFocusWithin(commentWrapper)
 const contentValid = computed(() => input.value?.trim().length > 0 && input.value != undefined)
+
 const showErrorMessage = ref(false)
+const toastType = ref('warning')
+const showToast = ref(false)
+const toastMessage = ref('')
 
 const handleFocus = () => {
   showErrorMessage.value = false
@@ -61,13 +85,40 @@ watch(focused, (val) => {
     showErrorMessage.value = false
   }
 })
-
-const submit = () => {
+const handleSubmit = async () => {
   if (!contentValid.value) {
     showErrorMessage.value = true
     return
   }
-  emits('submit', input.value)
+
+  const res = await commentsStore.createComment({
+    authorHash: userStore.sessionHash,
+    authorNickname: userStore.nickname,
+    postId: props.postId,
+    parentId: props.commentId,
+    content: input.value
+  })
+  showToast.value = true
+  if (res) {
+    toastMessage.value = 'Comment created successfully, waitng for moderation'
+    toastType.value = 'success'
+  } else {
+    toastMessage.value = 'Error creating comment'
+    toastType.value = 'danger'
+  }
+  watch(showToast, (newValue) => {
+    if (newValue) {
+      setTimeout(() => {
+        showToast.value = false
+      }, 3000)
+    }
+  })
+  // TODO remove this
+  console.log({
+    postId: props.postId,
+    parentId: props.commentId,
+    content: input.value
+  })
   input.value = ''
 }
 </script>
