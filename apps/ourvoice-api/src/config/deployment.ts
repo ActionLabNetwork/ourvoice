f7b5f0d9aae1adc16b3fc550d8929054dac0b6cc
knthmn <knthmn0@gmail.com>
2023-07-18 16:40:46 +1000

    ci(api): fix config file not able to be read in CI

--- apps/ourvoice-api/src/config/deployment.ts
+++ apps/ourvoice-api/src/config/deployment.ts
@@ -5,6 +5,9 @@ import { join } from 'path';
 const YAML_CONFIG_FILENAME = 'config.yml';
 
 export default () => {
+  if (process.env.NODE_ENV === 'test') {
+    return {};
+  }
   return yaml.load(
     readFileSync(join('../../config/', YAML_CONFIG_FILENAME), 'utf8'),
   ) as Record<string, any>;
