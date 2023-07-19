7189395ecd00600d72ad703d94b8ee000be0025b
knthmn <knthmn0@gmail.com>
2023-07-19 10:42:43 +1000

    feat(app): implement client side caching for posts page

--- apps/ourvoice-app/src/graphql/client/index.ts
+++ apps/ourvoice-app/src/graphql/client/index.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { ApolloClient, createHttpLink, InMemoryCache, type StoreObject } from '@apollo/client/core'
 import config from '@/config'
 
 // Set up GraphQL client
@@ -6,8 +6,21 @@ const httpLink = createHttpLink({
   uri: `${config.apiURL}/graphql` || 'http://localhost:3000/graphql'
 })
 
+ const apolloClientCache = new InMemoryCache()
 // Create the apollo client
 export const apolloClient = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: apolloClientCache
 })
+
+
+export function evictItem<T extends StoreObject>(o: T, fieldName?: keyof T & string) {
+  const cacheId = apolloClientCache.identify(o)
+  // If the object cannot be identified, reset the whole cache to prevent any inconsistency.
+  if (!cacheId) {
+    console.log(`Failed to evict cache for object: ${o}, clearing entire cache`)
+    apolloClient.resetStore()
+    return
+  }
+  apolloClientCache.evict({id: cacheId, fieldName})
+}
