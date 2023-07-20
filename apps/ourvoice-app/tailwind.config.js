914cc4cede9f2700bd4883c6b1eabe9f4841320f
Mingye1Li <87420384+Mingye1Li@users.noreply.github.com>
2023-07-20 10:01:00 +1000

    style(app): add theme congiguration

--- apps/ourvoice-app/tailwind.config.js
+++ apps/ourvoice-app/tailwind.config.js
@@ -1,22 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
+import fs from 'fs'
+import yaml from 'js-yaml'
+const theme = yaml.load(fs.readFileSync('./src/config/theme-config.yml', 'utf8'))
+// console.log(theme)
+
+export default {
   mode: 'jit', // Just in time compiler
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'ourvoice-purple': '#5267DF',
-        'ourvoice-red': '#FA5959',
-        'ourvoice-blue': '#242A45',
-        'ourvoice-grey': '#9194A2',
-        'ourvoice-white': '#f7f7f7'
-      }
-    },
-    fontFamily: {
-      Poppins: ['Poppins, sans-serif']
-    },
+    extend: theme.custom || theme.default,
     container: {
       center: true,
       padding: '1rem',
