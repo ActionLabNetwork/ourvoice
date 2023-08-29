80592d654c327d8b5aaf645b044c161a56b70889
knthmn <knthmn0@gmail.com>
2023-08-29 17:06:39 +1000

    feat(app): display category description in post list

--- apps/ourvoice-app/src/stores/categories.ts
+++ apps/ourvoice-app/src/stores/categories.ts
@@ -2,11 +2,10 @@ import { GET_CATEGORIES_QUERY } from './../graphql/queries/getCategories'
 import { defineStore } from 'pinia'
 import { apolloClient } from './../graphql/client/index'
 
-
 export interface CategoriesState {
-  data: { id: number; name: string; numPosts: number }[]
+  data: { id: number; name: string; numPosts: number; description: string }[]
   // selectedCategories: number[]
-  state: "initial" | "loading-initial" | "loaded" | "loading-more" | "error"
+  state: 'initial' | 'loading-initial' | 'loaded' | 'loading-more' | 'error'
   error: Error | undefined
   errorMessage: string | undefined
 }
@@ -15,14 +14,14 @@ export const useCategoriesStore = defineStore('categories', {
   state: (): CategoriesState => ({
     data: [],
     // selectedCategories: [],
-    state: "initial",
+    state: 'initial',
     error: undefined,
     errorMessage: undefined
   }),
 
   actions: {
     async fetchCategories() {
-      this.state = "loading-initial"
+      this.state = 'loading-initial'
       try {
         const { data } = await apolloClient.query({ query: GET_CATEGORIES_QUERY })
         const categories = data.categories
@@ -30,7 +29,7 @@ export const useCategoriesStore = defineStore('categories', {
           throw Error('Returned data is null')
         }
         this.data = categories.edges.map((edge) => edge.node)
-        this.state = "loaded"
+        this.state = 'loaded'
       } catch (error) {
         if (error instanceof Error) {
           this.error = error
@@ -38,7 +37,7 @@ export const useCategoriesStore = defineStore('categories', {
         if (error) {
           this.errorMessage = 'Failed to load categories. Please try again.'
         }
-        this.state = "error"
+        this.state = 'error'
       }
     }
   },
@@ -46,7 +45,7 @@ export const useCategoriesStore = defineStore('categories', {
   getters: {
     // TODO: for compatibility when refactoring. consider removing this field later
     loading(): boolean {
-      return this.state === "loading-initial" || this.state === "loading-more"  
+      return this.state === 'loading-initial' || this.state === 'loading-more'
     }
   }
 })
