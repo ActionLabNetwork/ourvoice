6152cc4e9d9f2303ab2c50ba432635ac648203f5
Christopher <christopherowenprawira@gmail.com>
2024-01-23 13:58:00 +1100

    build(app): add shadcn-vue as dependency

--- apps/ourvoice-app/tailwind.config.js
+++ apps/ourvoice-app/tailwind.config.js
@@ -1,3 +1,5 @@
+const animate = require('tailwindcss-animate')
+
 /** @type {import('tailwindcss').Config} */
 import fs from 'fs'
 import yaml from 'js-yaml'
@@ -7,7 +9,13 @@ const theme = yaml.load(fs.readFileSync('./src/config/theme-config.yml', 'utf8')
 export default {
   mode: 'jit', // Just in time compiler
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}'
+  ],
   theme: {
     extend: theme.custom || theme.default,
     container: {
@@ -20,6 +28,20 @@ export default {
         xl: '1440px',
         '2xl': '2560px'
       }
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: 0 },
+        to: { height: 'var(--radix-accordion-content-height)' }
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: 0 }
+      }
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out'
     }
   },
   plugins: [import('@tailwindcss/forms')]
